b769396ab8aa2885ee8876536027017933646e1b
Paulinstaller <paulinstaller@gmail.com>
2023-04-10 17:45:06 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  program that prints its name, followed by a new line
+ * @argc: arguments number
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
